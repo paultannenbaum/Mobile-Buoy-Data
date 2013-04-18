f55513e36c3e0c02a1c522907d2bd3df650d5cf5
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-04-17 22:59:59 -0700

    few small config changes

--- config.rb
+++ config.rb
@@ -2,9 +2,6 @@
 # Helpers
 ###
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
@@ -12,20 +9,20 @@
 #   end
 # end
 
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
 # Live reload
 activate :livereload
 
+# directories
 set :css_dir, 'css'
-
 set :js_dir, 'js'
-
 set :images_dir, 'img'
 
 # Build-specific configuration
 configure :build do
-  # Minify Javascript on build
+  activate :minify_css
   activate :minify_javascript
-
-  # Enable cache buster
   activate :cache_buster
 end
